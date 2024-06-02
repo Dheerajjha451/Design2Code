0a7d06b748f8ac15a01be119bc49087ed55ca268
dheerajjha451 <dheerajjha451@gmail.com>
2024-06-02 16:54:15 +0530

    update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
               Your Hub for  <span className="text-green-600">Project</span> Templates
             </h1>
             <p className="mt-6 text-xl text-gray-600">
-              Ready to kickstart your next big idea? Welcome to <span className="text-indigo-600">Design2Code</span>, where developers like you come together to showcase their <span className='text-blue-600'>design </span> and <span className="text-red-600">project</span> expertise. Whether you're creating a <span className="text-yellow-600">website</span>, <span className="text-blue-600">mobile app</span>, or exploring a new technology, find the <span className="text-purple-600">inspiration</span> and <span className="text-purple-600">resources</span> you need to bring your projects to life.
+              Ready to kickstart your next big idea? Welcome to <span className="text-indigo-600">Design2Code</span>, where developers like you come together to showcase their <span className='text-blue-600'>design </span> and <span className="text-red-600">project</span> expertise. Find the <span className="text-purple-600">inspiration</span> and <span className="text-purple-600">resources</span> you need to bring your projects to life.
             </p>
           </div>
           <div className="mt-12 text-center">
