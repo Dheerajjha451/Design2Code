6c5137ec3783535f3bdb1a47b4b48ab69eaf92cb
dheerajjha451 <dheerajjha451@gmail.com>
2024-06-04 14:28:38 +0530

    vercel speed

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
+        <SpeedInsights />
         <NextScript />
       </body>
     </Html>
