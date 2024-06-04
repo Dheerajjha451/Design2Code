e6bb5b8472a3b0a62b4eddfd9f33e2be193288a7
dheerajjha451 <dheerajjha451@gmail.com>
2024-06-04 14:34:44 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import Header from "@/components/Header";
 import "@/styles/globals.css";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SessionProvider } from "next-auth/react"
-
+import { Analytics } from '@vercel/analytics/react';
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
@@ -15,5 +15,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
         <Component {...pageProps} />
       </main>
     </SessionProvider>
+    <SpeedInsights />
+    <Analytics />
   </>
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
-        <SpeedInsights />
         <NextScript />
       </body>
     </Html>
