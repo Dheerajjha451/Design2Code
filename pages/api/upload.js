871a96b4fb719d7fc0b23f4dd73d8ad55809b5d4
dheerajjha451 <dheerajjha451@gmail.com>
2024-06-18 00:26:58 +0530

    format

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -25,7 +25,6 @@ export default async function handle(req, res) {
       folder: "design2code",
       public_id: `file_${Date.now()}`,
       resource_type: "auto",
-      format: "webp",  
       transformation: [
         { width: 800, height: 600, crop: "limit" },  
         { quality: "auto:good" },  
