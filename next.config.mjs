96fb18c79d2a3a9867001d8941627d5fc004eb1b
Dheerajjha451 <dheerajjha451@gmail.com>
2025-02-16 08:05:29 +0530

    Improve SEO

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com']
-  }
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap");
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
